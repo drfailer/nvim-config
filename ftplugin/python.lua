731c72986b48c3e02eb4bcb4cb5c9203c0339b6e
drfailer <drfailer.42@gmail.com>
2022-10-08 15:21:05 +0200

    fix

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,3 +1,3 @@
 vim.keymap.set('n', '<leader>cc', '<CMD>!python3 %<CR>', { noremap = true })
-vim.keymap.set('v', '<leader>cc', '<CMD>w !python3<CR>', { noremap = true })
-vim.keymap.set('v', '<leader>cl', 'V<CMD>w !python3<CR>', { noremap = true })
+vim.keymap.set('v', '<leader>cc', ':w !python3<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>cl', 'V:w !python3<CR>', { noremap = true })
