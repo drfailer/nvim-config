bc1f5a090fa1a61f47a0257c0db946fef63b6896
drfailer <drfailer.42@gmail.com>
2022-10-14 23:52:43 +0200

    fix comment header

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,1 +1,1 @@
-vim.keymap.set('n', '<leader>FH', 'O<Escape>80i-<Escape>yyjpk<CMD>center 80<CR>0r-lr-<CMD>set virtualedit=all<CR>078lr-lr-0<CDM>virtualedit=all<CR>', {noremap = true})
+vim.keymap.set('n', '<leader>FH', 'O<Escape>80i-<Escape>yyjpk<CMD>center 80<CR>0r-lr-<CMD>set virtualedit=all<CR>078lr-lr-0<CDM>virtualedit=<CR>', {noremap = true})
