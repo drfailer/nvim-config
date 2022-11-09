7119171e9c04e533b0d3002e4cc5693e901f25d5
drfailer <drfailer.42@gmail.com>
2022-11-09 12:06:38 +0100

    theme changes

--- lua/dfplugin/ui/theme.lua
+++ lua/dfplugin/ui/theme.lua
@@ -72,7 +72,7 @@ require('rose-pine').setup({
 
 -- tokyodark
 -- init.lua
-vim.g.tokyodark_transparent_background = false
+vim.g.tokyodark_transparent_background = true
 vim.g.tokyodark_enable_italic_comment = true
 vim.g.tokyodark_enable_italic = true
 vim.g.tokyodark_color_gamma = "1.0"
