8bcd87ac1677187e45fd01b9970e1bf140d8eb8a
drfailer <drfailer.42@gmail.com>
2022-12-28 09:52:20 +0100

    themes changes

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -28,39 +28,6 @@ require('onedark').setup {
     style = 'dark'
 }
 
--- rose-pine
-require('rose-pine').setup({
-	dark_variant = 'main', -- main moon
-	bold_vert_split = false,
-	dim_nc_background = false,
-	disable_background = false,
-	disable_float_background = false,
-	disable_italics = false,
-
-	groups = {
-		background = '#000000',
-		panel = 'surface',
-		border = 'highlight_med',
-		comment = 'muted',
-		link = 'iris',
-		punctuation = 'subtle',
-
-		error = 'love',
-		hint = 'iris',
-		info = 'foam',
-		warn = 'gold',
-
-		headings = {
-			h1 = 'iris',
-			h2 = 'foam',
-			h3 = 'rose',
-			h4 = 'gold',
-			h5 = 'pine',
-			h6 = 'foam',
-		}
-	},
-})
-
 -- vim.cmd("colorscheme onedark")
 -- vim.cmd('colorscheme rose-pine')
 vim.cmd("colorscheme gruvbox")
