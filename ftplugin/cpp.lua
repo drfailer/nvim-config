c1633e36f429dda115ca52e874f507dd44b7e792
drfailer <drfailer.42@gmail.com>
2023-01-15 11:25:48 +0100

    tweak method generation

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,3 +1,3 @@
 -- note: this could be done in a better way using treesitter
--- methode generation
-vim.keymap.set('n', '<leader>lgm', 'mzB"myt(f("pyi(?class<CR>w"cyiw/{<CR>%o<C-r>c::<C-r>m(<C-r>p)<Escape>o{}<Escape>`z', {noremap = true})
+-- methode generation (note: record a macro)
+vim.keymap.set('n', '<C-m>', 'mzB"myt(f("pyi(?class<CR>w"cyiw/{<CR>%o<C-r>c::<C-r>m(<C-r>p)<Escape>o{}<Escape>`zj^w', {noremap = true})
