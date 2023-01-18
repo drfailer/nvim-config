8cd9d153317d68c84f8b1fa8d8834ffe9302251c
drfailer <drfailer.42@gmail.com>
2023-01-18 13:16:01 +0100

    fix methode generation

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,3 +1,4 @@
 -- note: this could be done in a better way using treesitter
--- methode generation (note: record a macro)
-vim.keymap.set('n', '<C-m>', 'mzB"myt(f("pyi(?class<CR>w"cyiw/{<CR>%o<C-r>c::<C-r>m(<C-r>p)<Escape>o{}<Escape>`zj^w', {noremap = true})
+-- generate methodes
+-- TODO: implement a way to add it in the cpp file directly
+vim.keymap.set('n', '<C-m>', 'mz^"myt;?class<CR>w"cyiw/{<CR>%o<C-r>m<Esc>0f(Bi<C-r>c::<Esc>o{<CR>}<Esc>=ip`zj', {noremap = true})
