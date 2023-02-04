e6d7945677e44234e0b0c2f78527fa93512f05b0
drfailer <drfailer.42@gmail.com>
2023-02-04 20:13:00 +0100

    minor update

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,4 +1,4 @@
 -- note: this could be done in a better way using treesitter
 -- generate methodes
 -- TODO: implement a way to add it in the cpp file directly
-vim.keymap.set('n', '<C-m>', 'mz^"myt;?class<CR>w"cyiw/{<CR>%o<C-r>m<Esc>0f(Bi<C-r>c::<Esc>o{<CR>}<Esc>=ip`zj', {noremap = true})
+vim.keymap.set('n', '<C-m>', 'mz^"myt;?^class<CR>w"cyiw/{<CR>%o<CR><C-r>m<Esc>0f(Bi<C-r>c::<Esc>o{<CR>}<Esc>=ip`zj', {noremap = true})
