83bea3a43336d7814b0e9da9867e9e045c9bff88
drfailer <drfailer.42@gmail.com>
2023-02-27 14:31:18 +0100

    fix clang-format indent

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<leader>F', ':%!clang-format --style=Mozilla<cr>', {
+vim.keymap.set('n', '<leader>FF', ':%!clang-format --style=Mozilla<cr>', {
   noremap = true })
 vim.keymap.set('n', '<leader>cc', '<CMD>make<CR>', {noremap = true})
 
