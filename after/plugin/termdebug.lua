3681b35caae4dd8f97ef56b1aa729725206e32a1
drfailer <drfailer.42@gmail.com>
2023-07-17 17:59:26 +0200

    fix termdebug + snippets

--- after/plugin/termdebug.lua
+++ after/plugin/termdebug.lua
@@ -5,20 +5,22 @@
 -- enable termdebug
 vim.cmd("packadd termdebug")
 
-vim.keymap.set('n', '<F8>',       '<CMD>Over<CR>',     { noremap = true, buffer = true})
-vim.keymap.set('n', '<F9>',       '<CMD>Step<CR>',     { noremap = true, buffer = true})
-vim.keymap.set('n', '<F10>',      '<CMD>Continue<CR>', { noremap = true, buffer = true})
-vim.keymap.set('n', '<F11>',      '<CMD>Run<CR>',      { noremap = true, buffer = true})
-vim.keymap.set('n', '<F12>',      '<CMD>Stop<CR>',     { noremap = true, buffer = true})
-vim.keymap.set('n', '<leader>du', '<CMD>Until<CR>',    { noremap = true, buffer = true})
-vim.keymap.set('n', '<leader>db', '<CMD>Break<CR>',    { noremap = true, buffer = true})
-vim.keymap.set('n', '<leader>dc', '<CMD>Clear<CR>',    { noremap = true, buffer = true})
-vim.keymap.set('n', '<leader>dp', '<CMD>Evaluate<CR>', { noremap = true, buffer = true})
-vim.keymap.set('v', '<leader>dp', '<CMD>Evaluate<CR>', { noremap = true, buffer = true})
+vim.g.termdebug_wide = 1
+
+vim.keymap.set('n', '<F8>',       '<CMD>Over<CR>',     { noremap = true })
+vim.keymap.set('n', '<F9>',       '<CMD>Step<CR>',     { noremap = true })
+vim.keymap.set('n', '<F10>',      '<CMD>Continue<CR>', { noremap = true })
+vim.keymap.set('n', '<F11>',      '<CMD>Run<CR>',      { noremap = true })
+vim.keymap.set('n', '<F12>',      '<CMD>Stop<CR>',     { noremap = true })
+vim.keymap.set('n', '<leader>du', '<CMD>Until<CR>',    { noremap = true })
+vim.keymap.set('n', '<leader>db', '<CMD>Break<CR>',    { noremap = true })
+vim.keymap.set('n', '<leader>dc', '<CMD>Clear<CR>',    { noremap = true })
+vim.keymap.set('n', '<leader>dp', '<CMD>Evaluate<CR>', { noremap = true })
+vim.keymap.set('v', '<leader>dp', '<CMD>Evaluate<CR>', { noremap = true })
 
 vim.keymap.set('n', '<leader>dd',
 function()
   local exec_name = vim.fn.input('Executable: ', '', 'file')
   vim.cmd("Termdebug " .. exec_name)
 end,
-{ noremap = true, buffer = true })
+{ noremap = true })
