0c87785d225d75e9c54f5b68f5092260bf067002
drfailer <drfailer.42@gmail.com>
2024-04-20 21:07:51 +0200

    change vimwiki mapping

--- ftplugin/vimwiki.lua
+++ ftplugin/vimwiki.lua
@@ -1,1 +1,1 @@
-vim.keymap.set('n', '<leader>cc', '<CMD>!pandoc -V geometry:margin=1in % -o %:r.pdf; zathura %:r.pdf &<CR>', { noremap = true, buffer = true})
+vim.keymap.set('n', '<leader>co', '<CMD>!pandoc -V geometry:margin=1in % -o %:r.pdf; zathura %:r.pdf &<CR>', { noremap = true, buffer = true})
