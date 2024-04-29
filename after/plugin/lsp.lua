262ccb1b6dbfbcd45ebe3ca03ad4bdf86515d8ff
drfailer <drfailer.42@gmail.com>
2024-04-29 09:07:18 +0200

    update luasnip config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -110,11 +110,24 @@ cmp.setup({
 
 -- snipmate like snippet load
 require("luasnip.loaders.from_snipmate").lazy_load()
+local ls = require("luasnip")
 
--- luasnip mappings
-vim.cmd("imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'")
-vim.cmd("inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>")
-vim.cmd("snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>")
-vim.cmd("snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>")
-vim.cmd("imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'")
-vim.cmd("smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'")
+-- -- luasnip mappings
+vim.keymap.set({"i"}, "<Tab>", function()
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    else
+        local tabkey = ""
+        for i=1,vim.o.tabstop do
+            tabkey = tabkey.." "
+        end
+        vim.api.nvim_feedkeys(tabkey, "i", true)
+    end
+end, {silent = true})
+vim.keymap.set({"s"}, "<Tab>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
--- lua/dfconfig/packer.lua
+++ lua/dfconfig/packer.lua
@@ -84,7 +84,7 @@ return require('packer').startup(function()
       {'hrsh7th/cmp-nvim-lsp-signature-help'},
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
+      {'L3MON4D3/LuaSnip', tag = 'v2.*'},
       {'rafamadriz/friendly-snippets'},
     }
   }
