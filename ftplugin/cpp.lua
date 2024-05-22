664ea4ce2d152c82ad6d871146f0fab01e2fdddf
drfailer <drfailer.42@gmail.com>
2024-05-22 10:22:16 -0400

    improved getter and setter generation

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -10,7 +10,7 @@
 local yankClassName = '?^class<CR>w"cyiw'
 local yankMethod = '$F)v^"my'
 local yankVar = '"vyiw'
-local yankType = '^"tyt '
+local yankType = '^v/[=;]<CR>B?[a-zA-Z0-9>*]<CR>"ty'
 local cut = 'vipokd'
 
 -- generation
@@ -19,11 +19,13 @@ local genGetterDec = '<C-r>t get!<C-r>v() const;<Esc>?!<CR>"_xvU'
 local genSetterDec = 'void set!<C-r>v(<C-r>t);<Esc>?!<CR>"_xvU'
 local genGetterDef = '<C-r>t get!<C-r>v() const { return <C-r>v; }<Esc>?!<CR>"_xvU=='
 local genSetterDef = 'void set!<C-r>v(<C-r>t <C-r>v) { this-><C-r>v = <C-r>v; }<Esc>?!<CR>"_xvU=='
+local genGetterDef_ = '<C-r>t <C-r>v() const { return <C-r>v; }<Esc>^/_(<CR>"_x'
+local genSetterDef_ = 'void <C-r>v(<C-r>t <C-r>v) { this-><C-r>v = <C-r>v; }<Esc>^/_(<CR>"_x/_)<CR>"_x/_;<CR>"_x'
 local switchToSrc = ':e %:r.cpp<CR>'
 
 -- goto
 local insertEnd = 'GO<Esc>O'
-local insertPub = '/public:<CR>o'
+local insertPub = '/public:<CR>}O'
 local st = 'mz'
 local gt = '`z'
 
@@ -33,9 +35,9 @@ vim.keymap.set('n', '<C-m>',
   { noremap = true, buffer = true })
 -- getter
 vim.keymap.set('n', '<leader>lgg',
-  st .. yankVar .. yankType .. insertPub .. genGetterDef .. gt,
+  st .. yankVar .. yankType .. insertPub .. genGetterDef_ .. gt,
   { noremap = true, buffer = true })
 -- setter
 vim.keymap.set('n', '<leader>lgs',
-  st .. yankVar .. yankType .. insertPub .. genSetterDef .. gt,
+  st .. yankVar .. yankType .. insertPub .. genSetterDef_ .. gt,
   { noremap = true, buffer = true })
