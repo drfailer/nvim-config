7056cce997add68479c2df12f46f4553daa5293e
drfailer <drfailer.42@gmail.com>
2024-06-30 14:18:38 -0400

    fix: python header too long

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,4 +1,4 @@
 vim.keymap.set('n', '<leader>cc', '<CMD>!python3 %<CR>', { noremap = true, buffer = true })
 vim.keymap.set('v', '<leader>cc', ':w !python3<CR>', { noremap = true, buffer = true})
 vim.keymap.set('n', '<leader>cl', 'V:w !python3<CR>', { noremap = true, buffer = true})
-vim.keymap.set('n', '<leader>FH', 'O<Escape>80i#<Escape>yyjpk<CMD>center 80<CR>0r#<CMD>set virtualedit=all<CR>079lr#:set virtualedit=none<CR>', { noremap = true, buffer = true })
+vim.keymap.set('n', '<leader>FH', 'O<Escape>79i#<Escape>yyjpk<CMD>center 79<CR>0r#<CMD>set virtualedit=all<CR>079lr#:set virtualedit=none<CR>', { noremap = true, buffer = true })
