d472c0776a101502781b1b151530f33fede88e88
drfailer <drfailer.42@gmail.com>
2024-07-15 14:03:52 -0400

    try cmp cmdline

--- lua/dfconfig/lsp/cmp.lua
+++ lua/dfconfig/lsp/cmp.lua
@@ -34,6 +34,25 @@ cmp.setup({
   })
 })
 
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
 -- for lsp config
 cmp_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
 
