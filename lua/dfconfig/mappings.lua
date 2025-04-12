afddaa66bfbf0827788501a7c63eb60754ab2373
drfailer <drfailer.42@gmail.com>
2025-04-12 16:53:30 -0400

    remove old mappings

--- lua/dfconfig/mappings.lua
+++ lua/dfconfig/mappings.lua
@@ -159,24 +159,5 @@ vim.api.nvim_create_autocmd('filetype', {
 })
 
 
--- remap keys that are difficult to reach
-
--- insert
-vim.keymap.set('i', '<C-y>', '`', { noremap = true })
-vim.keymap.set('i', '<C-_>', '<C-v>\\', { noremap = true })
-
 -- remap the C-^
 vim.keymap.set('n', '<C-p>', '<C-^>', { noremap = true })
-
--- yet another escape cmd
-vim.keymap.set('v', '<C-Space>', '<Esc>', { noremap = true })
-
--- [ is difficult to reach on azerty keyboards
-vim.keymap.set('n', 'viq', 'vi[', { noremap = true })
-vim.keymap.set('n', 'vaq', 'va[', { noremap = true })
-vim.keymap.set('n', 'yiq', 'yi[', { noremap = true })
-vim.keymap.set('n', 'yaq', 'ya[', { noremap = true })
-vim.keymap.set('n', 'diq', 'di[', { noremap = true })
-vim.keymap.set('n', 'daq', 'da[', { noremap = true })
-vim.keymap.set('n', 'ciq', 'ci[', { noremap = true })
-vim.keymap.set('n', 'caq', 'ca[', { noremap = true })
