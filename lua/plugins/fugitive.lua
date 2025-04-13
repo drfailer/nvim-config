6676d428a7134ba4b1c290bc7bfcee8b9c83337d
drfailer <drfailer.42@gmail.com>
2025-04-12 22:19:12 -0400

    udate which key

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,15 +1,15 @@
 return {
     "tpope/vim-fugitive",
     keys = {
-        { "<leader>gs", "<CMD>G<cr>" },
-        { "<leader>gc", "<CMD>G commit<cr>" },
-        { "<leader>gp", "<CMD>G push<cr>" },
-        { "<leader>gL", "<CMD>G log<cr>" },
-        { "<leader>gm", ":G merge " },
-        { "<leader>gd", ":Gvdiffsplit!<cr>" },
-        { "<leader>gh", "<CMD>diffget //2<cr>" },
-        { "<leader>gl", "<CMD>diffget //3<cr>" },
-        { "<leader>gj", "]c" },
-        { "<leader>gk", "[c" },
+        { "<leader>gs", "<CMD>G<cr>", desc = "status" },
+        { "<leader>gc", "<CMD>G commit<cr>", desc = "commit" },
+        { "<leader>gp", "<CMD>G push<cr>", desc = "push" },
+        { "<leader>gL", "<CMD>G log<cr>", desc = "log" },
+        { "<leader>gm", ":G merge ", desc = "merge" },
+        { "<leader>gd", ":Gvdiffsplit!<cr>", desc = "diff split" },
+        { "<leader>gh", "<CMD>diffget //2<cr>", desc = "diff get left" },
+        { "<leader>gl", "<CMD>diffget //3<cr>", desc = "diff get right" },
+        { "<leader>gj", "]c", desc = "next diff" },
+        { "<leader>gk", "[c", desc = "next diff" },
     }
 }
