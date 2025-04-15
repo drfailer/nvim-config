6967c7da2475a43fcc6fb72fd68d86032a84f6cc
drfailer <drfailer.42@gmail.com>
2025-04-14 21:34:10 -0400

    terminal autocmd
    todo: there might be some things that could be interesting to do with it
    on windows.

--- lua/dfconfig/settings.lua
+++ lua/dfconfig/settings.lua
@@ -113,6 +113,14 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   command = [[set ft=java]],
 })
 
+vim.api.nvim_create_autocmd("TermOpen", {
+    group = vim.api.nvim_create_augroup("term-open", { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end
+})
+
 --------------------------------------------------------------------------------
 --                                  vimwiki                                   --
 --------------------------------------------------------------------------------
