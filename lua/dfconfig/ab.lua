2edbe7cfc7a1a7885a7759c48f0b9410d5dc2b10
drfailer <drfailer.42@gmail.com>
2025-07-02 08:27:30 -0400

    update ab

--- lua/dfconfig/ab.lua
+++ lua/dfconfig/ab.lua
@@ -2,14 +2,21 @@
 --                                ABREVIATION                                 --
 --------------------------------------------------------------------------------
 
+-- Programming
+vim.cmd("ab itn int")
+vim.cmd("ab typenaem typename")
+
+-- English
 vim.cmd("ab fonciton fonction")
 vim.cmd("ab foncitons fonctions")
-vim.cmd("ab itn int")
+vim.cmd("ab funciton function")
+vim.cmd("ab funcitons functions")
 vim.cmd("ab smth something")
-vim.cmd("ab mm même")
-vim.cmd("ab etre être")
-vim.cmd("ab controle contrôle")
-vim.cmd("ab notem notamment")
-vim.cmd("ab bib bibliothèque")
-vim.cmd("ab founit fournit")
-vim.cmd("ab typenaem typename")
+
+-- French
+-- vim.cmd("ab mm même")
+-- vim.cmd("ab etre être")
+-- vim.cmd("ab controle contrôle")
+-- vim.cmd("ab notem notamment")
+-- vim.cmd("ab bib bibliothèque")
+-- vim.cmd("ab founit fournit")
