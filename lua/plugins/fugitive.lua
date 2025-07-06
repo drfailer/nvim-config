a211a623479a5d47771d1c6ba6be831efd2a9e14
drfailer <drfailer.42@gmail.com>
2025-07-06 08:43:47 -0400

    go back to fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,25 +1,25 @@
-return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  keys = {
-    { "<leader>gs", function() require("neogit").open({ kind = "split", cwd = vim.fn.expand("%:p:h") }) end, desc = "open neogit" },
-  }
-}
-
 -- return {
---   "tpope/vim-fugitive",
+--   "NeogitOrg/neogit",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--   },
 --   keys = {
---     { "<leader>gs", "<CMD>G<cr>", desc = "status" },
---     { "<leader>gc", "<CMD>G commit<cr>", desc = "commit" },
---     { "<leader>gp", "<CMD>G push<cr>", desc = "push" },
---     { "<leader>gL", "<CMD>G log<cr>", desc = "log" },
---     { "<leader>gm", ":G merge ", desc = "merge" },
---     { "<leader>gd", ":Gvdiffsplit!<cr>", desc = "diff split" },
---     { "<leader>gh", "<CMD>diffget //2<cr>", desc = "diff get left" },
---     { "<leader>gl", "<CMD>diffget //3<cr>", desc = "diff get right" },
---     { "<leader>gj", "]c", desc = "next diff" },
---     { "<leader>gk", "[c", desc = "next diff" },
+--     { "<leader>gs", function() require("neogit").open({ kind = "split", cwd = vim.fn.expand("%:p:h") }) end, desc = "open neogit" },
 --   }
 -- }
+
+return {
+  "tpope/vim-fugitive",
+  keys = {
+    { "<leader>gs", "<CMD>G<cr>", desc = "status" },
+    { "<leader>gc", "<CMD>G commit<cr>", desc = "commit" },
+    { "<leader>gp", "<CMD>G push<cr>", desc = "push" },
+    { "<leader>gL", "<CMD>G log<cr>", desc = "log" },
+    { "<leader>gm", ":G merge ", desc = "merge" },
+    { "<leader>gd", ":Gvdiffsplit!<cr>", desc = "diff split" },
+    { "<leader>gh", "<CMD>diffget //2<cr>", desc = "diff get left" },
+    { "<leader>gl", "<CMD>diffget //3<cr>", desc = "diff get right" },
+    { "<leader>gj", "]c", desc = "next diff" },
+    { "<leader>gk", "[c", desc = "next diff" },
+  }
+}
