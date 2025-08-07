954d70232b1ee209e02da17d887fc113683c3520
drfailer <drfailer.42@gmail.com>
2025-08-07 15:17:02 -0400

    update lsp mappings

--- lua/dfconfig/lsp/lsp-config.lua
+++ lua/dfconfig/lsp/lsp-config.lua
@@ -20,26 +20,47 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     local opts = { noremap = true }
 
-    local tb = require("telescope.builtin")
-
     -- generic mappings
     vim.keymap.set('n', 'K', function()
       vim.lsp.buf.hover({ border = 'rounded', })
     end, { buffer = args.buf })
-    -- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gd", tb.lsp_definitions, opts)
+
+    -- g commands
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gR", vim.lsp.buf.references, opts)
+
+    -- goto
+    vim.keymap.set("n", "<leader>lgd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<leader>lgD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "<leader>lgr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>lgi", vim.lsp.buf.implementation, opts)
+
+    -- buffer change
+    vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "<leader>lF", vim.lsp.buf.format, opts)
-    vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "<leader>ln", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "<leader>lp", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>ls", tb.lsp_document_symbols, opts)
-    vim.keymap.set("n", "<leader>lS", tb.lsp_workspace_symbols, opts)
-    vim.keymap.set("n", "<leader>lR", tb.lsp_references, opts)
-    vim.keymap.set("n", "<leader>lfd", tb.diagnostics, opts)
-    vim.keymap.set("n", "<leader>lft", tb.lsp_type_definitions, opts)
-    vim.keymap.set("n", "<leader>lfi", tb.lsp_implementations, opts)
+
+    -- diagnostic
+    vim.keymap.set("n", "<leader>ldo", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "<leader>ldn", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<leader>ldp", vim.diagnostic.goto_prev, opts)
+
+    -- symbols
+    vim.keymap.set("n", "<leader>lsd", vim.lsp.buf.document_symbol, opts)
+    vim.keymap.set("n", "<leader>lsw", vim.lsp.buf.workspace_symbol, opts)
+
+    -- types
+    vim.keymap.set("n", "<leader>ltd", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<leader>lth", vim.lsp.buf.typehierarchy, opts)
+
+    -- calls
+    vim.keymap.set("n", "<leader>lci", vim.lsp.buf.incoming_calls, opts)
+    vim.keymap.set("n", "<leader>lco", vim.lsp.buf.outgoing_calls, opts)
+
+    -- workspace actions
+    vim.keymap.set("n", "<leader>lwl", vim.lsp.buf.list_workspace_folders, opts)
+    vim.keymap.set("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder, opts)
 
     -- signature help
     vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
