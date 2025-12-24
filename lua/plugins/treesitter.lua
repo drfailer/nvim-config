f42573c0c0972b0dae9fead94142133d5e0c5602
drfailer <drfailer.42@gmail.com>
2025-12-24 10:00:17 +0100

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,24 +4,11 @@ return {
         -- return vim.fn.executable('gcc') or vim.fn.executable('clang')
         return not (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1)
     end,
+    lazy = false,
     config = function()
-        require("nvim-treesitter.configs").setup {
-            -- A list of parser names, or "all"
-            ensure_installed = { "c", "lua", "rust" },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            auto_install = true,
-
-            -- List of parsers to ignore installing (for "all")
-            ignore_install = { "latex", "tex" },
-
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        }
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'c', 'c++', 'asm', 'odin' },
+            callback = function() vim.treesitter.start() end,
+        })
     end
 }
