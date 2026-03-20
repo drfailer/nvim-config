3f29af1dcfc9d5ea913a953f88e7f5a4c996a4bd
drfailer <drfailer.42@gmail.com>
2026-03-20 13:42:51 -0400

    move odin compiler options

--- compiler/odin.lua
+++
@@ -1,2 +0,0 @@
-vim.opt_local.errorformat = "%f(%l:%c) %m"
-vim.opt_local.makeprg = "if [ -f build.sh ]; then ./build.sh $*; elif [ -f ./Makefile ]; then make $*; else odin build % -file; fi"
--- ftplugin/odin.lua
+++ ftplugin/odin.lua
@@ -1,1 +1,2 @@
-vim.cmd("compiler odin")
+vim.opt_local.errorformat = "%f(%l:%c) %m"
+vim.opt_local.makeprg = "if [ -f build.sh ]; then ./build.sh $*; elif [ -f ./Makefile ]; then make $*; else odin build % -file; fi"
