821e6f98c0eb3ca03ea70f69c137cd3b1777a1be
drfailer <drfailer.42@gmail.com>
2026-04-12 11:07:59 -0400

    disable tree sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,6 @@
-if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
-    return
-end
-
-vim.pack.add({"https://github.com/nvim-treesitter/nvim-treesitter.git"})
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'c', 'cpp', 'asm', 'odin' },
-    callback = function() vim.treesitter.start() end,
-})
+-- vim.pack.add({"https://github.com/nvim-treesitter/nvim-treesitter.git"})
+--
+-- vim.api.nvim_create_autocmd('FileType', {
+--     pattern = { 'c', 'cpp', 'asm', 'odin' },
+--     callback = function() vim.treesitter.start() end,
+-- })
